591c9e2ded80421ef301b0f2073bcb23e4df123e
Achinta <achintasiddhanta0304@gmail.com>
2026-05-05 00:22:49 +0530

    implement app.tsx

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,9 +1,42 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SignInPage from "./features/auth/SignInPage";
+import AuthLayout from "./components/layouts/AuthLayout";
+import Dashboard from "./components/pages/Dashboard";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClinet = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      staleTime: 1000 * 60 * 5,
+      refetchOnWindowFocus: false
+    }
+  }
+})
+
+const router = createBrowserRouter([
+  {
+    path: "/sign-in",
+    element: <SignInPage />
+  },
+  {
+    path: "/",
+    element: <AuthLayout />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />
+        
+      }
+    ]
+  }
+])
+
 const App = () => {
   return (
-    <main>
-      <div>Hello, World!</div>
-      <div>This is admin app</div>
-    </main>
+    <QueryClientProvider client={queryClinet}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   );
 };
 
